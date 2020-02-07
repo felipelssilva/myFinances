002f1eb920af943896857fa0dde7e7764e8797cc
Felipe Luis <felipe.silva@fh.com.br>
2020-02-07 09:34:53 -0300

    remove all php structure

--- App/Views/Home/Home-View.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('ABSPATH')) exit; ?>
-
-
-<div class="wrap">
-
-	<p>Olá, <br>
-	Se você está visualizando esta página, seu sistema PHP com padrão MVC está funcionando.</p>
-
-</div> <!-- .wrap -->
